ec53030fb95c5dfff9f2ecc009f0be5822f2c41a
hoaiphuong123 <hp34nam@gmail.com>
2022-08-10 17:37:43 +0700

    useeffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import './components/mycss.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+export default function App() {
+  const [color, setColor] = useState('green');
+  const [time, setTime] = useState(0);
+  const [data, setData] = useState([]);
+  const [stop, setStop] = useState(false);
+
+  const handlerColorStop = () => {
+    setColor('red');
+    setStop(!stop);
+  };
+
+  const handlerColorReset = () => {
+    setTime(0);
+    setColor('green');
+    setStop(stop);
+  };
+  useEffect(() => {
+    const getQues = async () => {
+      if (time % 5 === 1) {
+        await fetch('https://catfact.ninja/fact')
+          .then((res) => res.json())
+          .then((data) => {
+            setData(data);
+          });
+      }
+    };
+    getQues().catch((e) => console.log(e));
+  }, [time]);
+  console.log(data);
+
+  useEffect(() => {
+    if (stop === true) {
+      const timerId = setInterval(() => {
+        setTime((prevState) => prevState + 1);
+      }, 1000);
+      return () => clearInterval(timerId);
+    }
+  }, [stop]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Vi du UseEffect() Hook</h1>
+      <p>Time :{time}</p>
+      <div className="button">
+        <button className="spa" type="button" style={{ backgroundColor: color, width: '200px', height: ' 40px' }} onClick={handlerColorStop}>
+          Stop
+        </button>
+      </div>
+      <div>
+        <button className="spa" type="button" style={{ backgroundColor: '#FFE4E1', width: '200px', height: ' 40px' }} onClick={handlerColorReset}>
+          Reset
+        </button>
+      </div>
+      <p>{data.fact}</p>
     </div>
   );
 }
-
-export default App;
